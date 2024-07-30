f36a69ef06903b0ce14637f60563fccbdafbee73
Lucas Vizeu <nota10.lucas@gmail.com>
2024-07-30 10:55:21 -0300

    Inicial comit

---
+++ app/src/main/java/com/example/appods/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.appods;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void goMenu(View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goCN(View view) {
+        setContentView(R.layout.activity_ch2);
+    }
+
+    public void goCH(View view) {
+        setContentView(R.layout.activity_ch2);
+    }
+
+    public void goLG(View view) {
+        setContentView(R.layout.activity_ch2);
+    }
+
+    public void goTC(View view) {
+        setContentView(R.layout.activity_ch2);
+    }
+
+    public void goMat(View view) {
+        setContentView(R.layout.activity_ch2);
+    }
+
+    public void goPVPV(View view) {
+        setContentView(R.layout.activity_ch2);
+    }
+
+
+}
