b662c03641f7fff717696cce7d6f165e6c7ff186
gubartzen <108465498+gubartzen@users.noreply.github.com>
2024-08-01 11:39:48 -0300

    Activities 5,6,7,8 com seus arquivos java do botão home

--- app/src/main/java/com/example/appods/CH_Activity8.java
+++ app/src/main/java/com/example/appods/CH_Activity8.java
@@ -5,12 +5,12 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CH_Activity2 extends AppCompatActivity {
+public class CH_Activity8 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ch2);
+        setContentView(R.layout.activity_ch8);
     }
     public void goMenu(View view) {
         setContentView(R.layout.activity_main);
