c1a1f08bdda61f686f13a98c781adc239848ffc3
Lucas Vizeu <nota10.lucas@gmail.com>
2024-08-06 11:18:08 -0300

    limpando arquivos

--- app/src/main/java/com/example/appods/CH_Activity3.java
+++ app/src/main/java/com/example/appods/CH_Activity3.java
@@ -5,12 +5,12 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CH_Activity7 extends AppCompatActivity {
+public class CH_Activity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ch7);
+        setContentView(R.layout.activity_ch3);
     }
     public void goMenu(View view) {
         setContentView(R.layout.activity_main);
